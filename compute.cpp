19123096cf09cef37fedef7531ea0fc3dbaf02e6
jli12653 <124959187+jli12653@users.noreply.github.com>
2023-03-01 22:54:56 -0500

    Update compute.cpp

--- compute.cpp
+++ compute.cpp
@@ -10,8 +10,9 @@
 void compute_fn(double* A, double B, double C) {
   //(*A) = (*A) * B + C;
   //(*A) = C / (*A);
-  (*A) = sqrt(*A);
-  //(*A) = sin(*A);
+  //(*A) = sqrt(*A);
+  (*A) = sin(*A);
+  //(*A) = cos(*A);
 }
 
 int main(int argc, char** argv) {
